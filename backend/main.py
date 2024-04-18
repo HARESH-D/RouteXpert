56413d5d559f89346abe7ff9ba4017a060695183
hareshdinakar <hareshdinakar@gmail.com>
2024-04-18 21:50:09 +0530

    frontend update

--- backend/main.py
+++ backend/main.py
@@ -14,21 +14,9 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+    return templates.TemplateResponse("index.html", {"request": request})
 
-# @app.get("/geocode/{location}", response_model=dict)
-# async def geocode_location(location: str):
-#     # Use Bing Maps Location API to get the coordinates of the location.
-#     bing_maps_key = "AqesDSP1QCJfuPJJ0PeaaRPGCFr2gou-EVTJEgM5_d_aNCw80OGn1BfVXAZRqvWk"
-#     geocode_request = f"http://dev.virtualearth.net/REST/v1/Locations/{location}?key={bing_maps_key}"
+@app.get("/transportation_planner", response_class=HTMLResponse)
+async def transportation_planner(request: Request):
+    return templates.TemplateResponse("transportation_planner.html", {"request": request})
 
-#     async with httpx.AsyncClient() as client:
-#         response = await client.get(geocode_request)
-
-#         if response.status_code == 200:
-#             data = response.json()
-#             coordinates = data["resourceSets"][0]["resources"][0]["point"]["coordinates"]
-#             result = {"latitude": coordinates[0], "longitude": coordinates[1]}
-#             return jsonable_encoder(result)
-#         else:
-#             return {"error": f"Error geocoding location: {response.text}"}
